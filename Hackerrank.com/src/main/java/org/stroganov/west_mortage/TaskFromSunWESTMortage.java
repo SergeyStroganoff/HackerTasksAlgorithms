366b3e2ba51e72e98199e11c70da988a8000daf6
Aspire5_Stroganow <111sergey90@gmail.com>
2024-03-15 11:20:51 -0500

    TaskFromSunWESTMortage is done

--- Hackerrank.com/src/main/java/org/stroganov/west_mortage/TaskFromSunWESTMortage.java
+++ Hackerrank.com/src/main/java/org/stroganov/west_mortage/TaskFromSunWESTMortage.java
@@ -1,26 +1,65 @@
 package org.stroganov.west_mortage;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedHashMap;
-import java.util.List;
+import java.util.*;
 
 public class TaskFromSunWESTMortage {
 
-    public class Solution {
-        public static void main(String args[] ) throws Exception {
-            /* Enter your code here. Read input from STDIN. Print output to STDOUT */
-            BufferedReader bufferedReader = new  BufferedReader(new InputStreamReader(System.in));
-            String inputSrting = bufferedReader.readLine();
-            LinkedHashMap<String, List<String>> hashMatrix  = new LinkedHashMap<>();
-            String [] rowBuffer = inputSrting.split(";");
-            for (String rowString : rowBuffer) {
-                String [] rowValuesAndColumnName = rowString.split(":");
-                hashMatrix.put(rowValuesAndColumnName[1], Arrays.asList( rowValuesAndColumnName[0].split(",")));
-            }
-
+    public static String getStringFromConsole() {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String inputString;
+        try {
+            inputString = bufferedReader.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+        return inputString;
     }
 
+    public String createHTMLTable(String inputString) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+        HashMap<String, List<String>> rowsValuesMap = new HashMap<>();
+        List<String> columnNames = new ArrayList<>();
+        columnNames.add("-");
+        String[] rowBuffer = inputString.split(";");
+        for (String rowString : rowBuffer) {
+            String[] rowValuesAndColumnName = rowString.split(":");
+            columnNames.add(rowValuesAndColumnName[1]);
+            List<String> rowValues = Arrays.asList(rowValuesAndColumnName[0].split(","));
+            for (int i = 0; i < rowValues.size() - 1; i += 2) {
+                List<String> oneValueOfColumnAsList = new ArrayList<>(Collections.singletonList(rowValues.get(i + 1).trim()));
+                rowsValuesMap.merge(rowValues.get(i).trim(), oneValueOfColumnAsList, (oldValue, newValue) -> {
+                    oldValue.addAll(oneValueOfColumnAsList);
+                    return oldValue;
+                });
+            }
+        }
+
+        for (Map.Entry<String, List<String>> entry : rowsValuesMap.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+
+
+        StringBuilder result = new StringBuilder("<table border=\"1\">");
+        result.append("<tr>");
+        for (String columnName : columnNames) {
+            result.append("<th>").append(columnName).append("</th>");
+        }
+        result.append("</tr>");
+        for (Map.Entry<String, List<String>> entry : rowsValuesMap.entrySet()) {
+            result.append("<tr>");
+            result.append("<td>").append(entry.getKey()).append("</td>");
+            for (String value : entry.getValue()) {
+                result.append("<td>").append(value).append("</td>");
+            }
+            result.append("</tr>");
+        }
+        result.append("</table>");
+        System.out.println(result.toString());
+        return result.toString();
+    }
 }
+
+
--- Hackerrank.com/src/main/java/org/stroganov/west_mortage/readMe.md
+++ Hackerrank.com/src/main/java/org/stroganov/west_mortage/readMe.md
@@ -52,3 +52,8 @@
     </tr>
 </table>
 
+<table border="1"><tr><th>-</th><th>column1</th><th> column2</th><th> column3</th></tr><tr><td>1</td><td>100</td><td>50</td><td>10</td></tr><tr><td>2</td><td>200</td><td>150</td><td>20</td></tr><tr><td>3</td><td>303</td><td>250</td><td>30</td></tr><tr><td>4</td><td>400</td><td>350</td><td>40</td></tr><tr><td>5</td><td>500</td><td>450</td><td>50</td></tr><tr><td>6</td><td>600</td><td>550</td><td>60</td></tr></table>
+
+
+
+
