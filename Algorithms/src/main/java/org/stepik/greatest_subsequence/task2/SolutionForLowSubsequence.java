ce6606851fa45b08a42f30534bbf3de3b3bf8bd2
Aspire5_Stroganow <111sergey90@gmail.com>
2024-04-01 19:22:53 -0500

    Solution gotten;

--- Algorithms/src/main/java/org/stepik/greatest_subsequence/task2/SolutionForLowSubsequence.java
+++ Algorithms/src/main/java/org/stepik/greatest_subsequence/task2/SolutionForLowSubsequence.java
@@ -32,12 +32,12 @@ public class SolutionForLowSubsequence {
         int[] memoryArray = new int[array.length]; //massive for save subsequence
         Arrays.fill(indexes, Integer.MAX_VALUE);
         Arrays.fill(memoryArray, Integer.MIN_VALUE);
-       // memoryArray[0] = array[0]; //first element of subsequence
-      //  indexes[0] = 0; //index of first element of subsequence
+        memoryArray[0] = array[0]; //first element of subsequence
+        indexes[0] = 0; //index of first element of subsequence
         int lengthSubseq = -1; //length of subsequence
         int indexOfLongestSubsequence = 0;
         for (int i = 1; i < array.length; ++i) { //start from second element
-            indexes[i] = ceilIndex(memoryArray, 0, indexOfLongestSubsequence + 1, array[i]); //find ceil index for current element in subsequence array and save it in indexes array for current element of array sequence
+            indexes[i] = ceilIndexMy(memoryArray, 0, indexOfLongestSubsequence, array[i]); //find ceil index for current element in subsequence array and save it in indexes array for current element of array sequence
             if (lengthSubseq <= indexes[i]) {
                 lengthSubseq = indexes[i];
                 indexOfLongestSubsequence = i;
@@ -120,26 +120,24 @@ public class SolutionForLowSubsequence {
         return floorIndex;
     }
 
-    public static int ceilIndex(int[] subsequence, int startLeft, int startRight, int key) { //find ceil index for current element in subsequence array
-        int left = startLeft;
-        int right = startRight;
-        int mid;
+    public static int ceilIndexMy(int[] subsequence, int left, int right, int key) { //find ceil index for current element in subsequence array
+        int mid = (right - left) / 2;
         int ceilIndex = 0;
         boolean ceilIndexFound = false;
-        for (mid = (left + right) / 2; left <= right && !ceilIndexFound; mid = (left + right) / 2) { //binary search
-            if (subsequence[mid] < key) { //if current element of subsequence array is greater than current element of array sequence
+
+        while (left <= right) { // Use a while loop instead of a for loop for binary search
+            if (subsequence[mid] == key) {// If key is found, it is also the ceil
+                //  subsequence[mid + 1] = key;
+                ceilIndex = mid + 1;
+                left = mid + 1;
+                ceilIndexFound = true;
+            } else if (subsequence[mid] < key) { // Move left pointer to search right half
                 right = mid - 1;
-            } else if (subsequence[mid] == key) {
-                subsequence[mid + 1] = key;
-                ceilIndex = mid + 1;
-                ceilIndexFound = true;
-            } else if (mid + 1 >= right && subsequence[mid + 1] <= key) {
-                subsequence[mid + 1] = key;
-                ceilIndex = mid + 1;
-                ceilIndexFound = true;
-            } else {
+            } else { // Move right pointer to search left half
+                ceilIndex = mid; // Update ceilIndex to current mid (potential ceil)
                 left = mid + 1;
             }
+            mid = left + (right - left) / 2;
         }
         if (!ceilIndexFound) {
             if (mid == left) {
@@ -149,7 +147,40 @@ public class SolutionForLowSubsequence {
                 subsequence[mid + 1] = key;
                 ceilIndex = mid + 1;
             }
+        } else {
+            subsequence[ceilIndex] = key;
         }
         return ceilIndex;
     }
+
+
+        public static int ceilIndex ( int[] subsequence, int startLeft, int startRight, int key)
+        { //find ceil index for current element in subsequence array
+            int left = startLeft;
+            int right = startRight;
+            int mid;
+            int ceilIndex = 0;
+            boolean ceilIndexFound = false;
+            for (mid = (left + right) / 2; left <= right && !ceilIndexFound; mid = (left + right) / 2) { //binary search
+                if (subsequence[mid] < key) { //if current element of subsequence array is greater than current element of array sequence
+                    right = mid - 1;
+                } else if (subsequence[mid] == key) {
+                    subsequence[mid + 1] = key;
+                    ceilIndex = mid + 1;
+                    ceilIndexFound = true;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            if (!ceilIndexFound) {
+                if (mid == left) {
+                    subsequence[mid] = key;
+                    ceilIndex = mid;
+                } else {
+                    subsequence[mid + 1] = key;
+                    ceilIndex = mid + 1;
+                }
+            }
+            return ceilIndex;
+        }
 }
--- Algorithms/src/test/java/org/stepik/greatest_subsequence/task2/SolutionForLowSubsequenceTest.java
+++ Algorithms/src/test/java/org/stepik/greatest_subsequence/task2/SolutionForLowSubsequenceTest.java
@@ -8,11 +8,19 @@ import java.util.List;
 class SolutionForLowSubsequenceTest {
 
     @Test
-    void getLowestSubSequenceNLLogN() {
-        int[] array = {10,1,6,6,0,5,4,8,8,2,1,0,5};
+    void getLowestSubSequenceNLLogN_TestTheSameNumbers() {
+        int[] array = {2, 20, 20, 20, 20, 20, 20};
         SolutionForLowSubsequence solution = new SolutionForLowSubsequence();
         List<Integer> subsequence = solution.getLowestSubSequenceNLLogN(array);
-        Assertions.assertEquals(List.of(3, 4, 6, 7, 8, 9, 10, 11), subsequence);
+        Assertions.assertEquals(List.of(2, 3, 4, 5, 6, 7), subsequence);
+    }
+
+    @Test
+    void getLowestSubSequenceNLLogN() {
+        int[] array = {5, 1, 6, 6, 0, 5, 4, 8, 8, 2, 1, 0, 5};
+        SolutionForLowSubsequence solution = new SolutionForLowSubsequence();
+        List<Integer> subsequence = solution.getLowestSubSequenceNLLogN(array);
+        Assertions.assertEquals(List.of(3, 4, 6, 7, 10, 11, 12), subsequence);
     }
 
     @Test
@@ -21,5 +29,13 @@ class SolutionForLowSubsequenceTest {
         SolutionForLowSubsequence solution = new SolutionForLowSubsequence();
         List<Integer> subsequence = solution.getLowestSubSequenceNLLogN(array);
         Assertions.assertEquals(List.of(2, 6, 7, 8, 9), subsequence);
+    }
+
+    @Test
+    void getLowestSubSequenceNLLogN_Test16() {
+        int[] array = {5, 8, 5, 7, 4, 7, 4, 6, 3, 6, 20, 20, 20, 20, 20, 20};
+        SolutionForLowSubsequence solution = new SolutionForLowSubsequence();
+        List<Integer> subsequence = solution.getLowestSubSequenceNLLogN(array);
+        Assertions.assertEquals(List.of(11, 12, 13, 14, 15, 16), subsequence);
     }
 }
