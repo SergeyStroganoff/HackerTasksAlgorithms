63cf4582f30c9dae7de1d4ed7a901be37e7041c9
homer_glen <prokitchensdesign@gmail.com>
2024-04-25 13:32:56 -0700

    Marshrut task solved;

--- Algorithms/src/main/java/org/stroganov/marshrut_finder/MarshrutFinder.java
+++ Algorithms/src/main/java/org/stroganov/marshrut_finder/MarshrutFinder.java
@@ -1,20 +1,19 @@
 package org.stroganov.marshrut_finder;
 
+import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class MarshrutFinder {
 
     public static void main(String[] args) {
         StringBuilder stringBuilder = new StringBuilder();
         HashMap<String, String> rightOrder = new HashMap<>();
-        HashMap<String, String> wrongOrder = new HashMap<>();
         String marshruts = null;
+        File file = new File("");
+        String absolutePath = file.getAbsolutePath();
         try {
-            marshruts = FileReaderUtil.readFile("marshrutlist.txt");
+            marshruts = FileReaderUtil.readFile(absolutePath + File.separator + "marshrutlist.txt");
         } catch (IOException e) {
             System.out.println(e);
             System.exit(1);
@@ -25,11 +24,28 @@ public class MarshrutFinder {
         for (String pair : marshrutPairs) {
             String[] pairArray = pair.split(":");
             rightOrder.put(pairArray[0], pairArray[1]);
-            rightOrder.put(pairArray[1], pairArray[0]);
+
+        }
+        Collection<String> endPoints = rightOrder.values();
+        String firstCity;
+        String secondCity = "";
+        for (Map.Entry<String, String> entry : rightOrder.entrySet()) {
+            if (!endPoints.contains(entry.getKey())) {
+                firstCity = entry.getKey();
+                secondCity = entry.getValue();
+                stringBuilder.append(firstCity).append(" -> ");
+                stringBuilder.append(secondCity).append(" -> ");
+            }
         }
 
-        for (Map.Entry<String, String> entry : rightOrder.entrySet()) {
-            //todo
+        while (!secondCity.isEmpty()) {
+            if (rightOrder.containsKey(secondCity)) {
+                secondCity = rightOrder.get(secondCity);
+                stringBuilder.append(secondCity).append(" -> ");
+            } else {
+                secondCity = "";
+            }
         }
+        System.out.println(stringBuilder.toString());
     }
 }
