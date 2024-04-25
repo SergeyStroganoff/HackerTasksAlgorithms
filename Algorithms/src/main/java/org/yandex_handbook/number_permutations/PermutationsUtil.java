2ec0be2962031b26e7ef579d5593ef2eb3690619
Aspire5_Stroganow <111sergey90@gmail.com>
2024-04-25 16:57:46 -0500

    Combinations and Permutations is done.

---
+++ Algorithms/src/main/java/org/yandex_handbook/number_permutations/PermutationsUtil.java
@@ -0,0 +1,28 @@
+package org.yandex_handbook.number_permutations;
+
+import org.stroganov.factorial.FactorialMath;
+
+import java.util.Scanner;
+
+public class PermutationsUtil {
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        System.out.println(FactorialMath.factorial(n));
+    }
+
+    // Метод возвращает факториал числа
+    // n! = 1 * 2 * 3 * ... * n
+    // n! = n * (n-1)/2
+    // 0! = 1
+
+    public static int factorial(int n) {
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
