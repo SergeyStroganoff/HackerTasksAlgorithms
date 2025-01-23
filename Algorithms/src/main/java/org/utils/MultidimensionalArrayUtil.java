8cbbec89222c73ab544e54777586f0fd6db791b8
Aspire5_Stroganow <111sergey90@gmail.com>
2025-01-22 22:17:49 -0500

    Rock Game Second Solution is done;

--- Algorithms/src/main/java/org/utils/MultidimensionalArrayUtil.java
+++ Algorithms/src/main/java/org/utils/MultidimensionalArrayUtil.java
@@ -5,7 +5,7 @@ public class MultidimensionalArrayUtil {
     public static void print(boolean[][] array) {
         for (boolean[] row : array) {
             for (boolean element : row) {
-                System.out.print(element + " ");
+                System.out.print((String.valueOf(element).charAt(0) == 't' ? "+" : "-") + " ");
             }
             System.out.println();
         }
