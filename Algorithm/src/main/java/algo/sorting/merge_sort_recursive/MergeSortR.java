2f449ed8d0b93bd5dc2eee42d2b0bd462d940988
Aspire5_Stroganow <111sergey90@gmail.com>
2025-02-05 12:17:16 -0500

    Merge Sort FirstVersion

--- Algorithm/src/main/java/algo/sorting/merge_sort_recursive/MergeSortR.java
+++ Algorithm/src/main/java/algo/sorting/merge_sort_recursive/MergeSortR.java
@@ -1,5 +1,7 @@
 package algo.sorting.merge_sort_recursive;
 
+import java.util.Arrays;
+
 /**
  * Merge Sort — это алгоритм сортировки, который использует подход «разделяй и властвуй» (divide and conquer). Он рекурсивно разбивает массив на две половины, сортирует каждую половину, а затем объединяет (сливает) их в один отсортированный массив.
  * Основные шаги алгоритма:
@@ -10,4 +12,48 @@ package algo.sorting.merge_sort_recursive;
  * Например, подмассивы [27, 38] и [3, 43] объединяются в [3, 27, 38, 43].
  */
 public class MergeSortR {
+    public static void main(String[] args) {
+        int[] array = {-2, 4, 5, -1, 0, 3, -1, -7, -10, 12, 3, 7};
+        int[] sortedArray = mergeSortRecursive(array);
+        System.out.println(Arrays.toString(sortedArray));
+    }
+
+
+    // Оптимизация путем передачи не новых массивов в рекурсию (половинок), а передавать только индексы старого массива
+    private static int[] mergeSortRecursive(int[] array) {
+        if (array.length < 2) {
+            return array;
+        }
+        int middle = array.length / 2;
+        int[] arrayOne = mergeSortRecursive(Arrays.copyOfRange(array, 0, middle));
+        int[] arrayTwo = mergeSortRecursive(Arrays.copyOfRange(array, middle, array.length));
+        array = arraysMergeASC(arrayOne, arrayTwo);
+        return array;
+    }
+
+
+    private static int[] arraysMergeASC(int[] arrayOne, int[] arrayTwo) {
+        int[] result = new int[arrayOne.length + arrayTwo.length];
+        int firstPointer = 0;
+        int secondPointer = 0;
+        int counter = 0;
+        while (firstPointer < arrayOne.length && secondPointer < arrayTwo.length) {
+            if (arrayOne[firstPointer] < arrayTwo[secondPointer]) {
+                result[counter] = arrayOne[firstPointer];
+                firstPointer++;
+            } else {
+                result[counter] = arrayTwo[secondPointer];
+                secondPointer++;
+            }
+            counter++;
+        }
+        if (firstPointer != arrayOne.length) {
+            System.arraycopy(arrayOne, firstPointer, result, counter, arrayOne.length - firstPointer);
+        }
+        if (secondPointer != arrayTwo.length) {
+            System.arraycopy(arrayTwo, secondPointer, result, counter, arrayTwo.length - secondPointer);
+        }
+        return result;
+    }
 }
+
