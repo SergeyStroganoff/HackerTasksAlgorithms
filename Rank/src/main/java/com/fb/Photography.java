d1f8c2b1207bd36bf78a4d520ccd194ef64d2e9d
Aspire5_Stroganow <111sergey90@gmail.com>
2025-02-16 11:12:11 -0500

    # fb - Photography - second way solution

--- Rank/src/main/java/com/fb/Photography.java
+++ Rank/src/main/java/com/fb/Photography.java
@@ -1,5 +1,8 @@
 package com.fb;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
 /**
  * You are given:
  * A string C of length N, where each character represents a cell in a sequence.
@@ -25,7 +28,7 @@ public class Photography {
         String testString = "PABPBA";
         String testString2 = ".PBAAP.B";
         // System.out.println(getArtisticPhotographCount(6, testString, 1, 3));
-        System.out.println(getArtisticPhotographCount(8, testString2, 1, 3));
+        System.out.println(getArtisticPhotographCountEffective(8, testString2, 1, 3));
 
     }
 
@@ -47,6 +50,44 @@ public class Photography {
                                 result++;
                             }
                         }
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * runs in O(1) on average,
+     * but when combined in a loop, the total cost is O(Y - X) per character,
+     * leading to O(N * (Y - X)) complexity.
+     */
+
+    public static int getArtisticPhotographCountEffective(int N, String C, int X, int Y) {
+        // Write your code here
+        int validCombination = getValidCombinationEffective(C, X, Y, 'P', 'A', 'B');
+        validCombination += getValidCombinationEffective(C, X, Y, 'B', 'A', 'P');
+        return validCombination;
+    }
+
+    public static int getValidCombinationEffective(String C, int X, int Y, char first, char second, char third) {
+        int result = 0;
+        HashSet<Integer> pSet = new HashSet<>();
+        HashMap<Integer, Integer> aSet = new HashMap<>();
+        HashSet<Integer> bSet = new HashSet<>();
+        for (int i = 0; i < C.length(); i++) {
+            if (C.charAt(i) == first) {
+                pSet.add(i);
+            } else if (C.charAt(i) == second && !pSet.isEmpty()) {
+                for (int n = X; n <= Y; n++) {
+                    if (pSet.contains(i - n)) {
+                        aSet.merge(i, 1, Integer::sum);
+                    }
+                }
+            } else if (C.charAt(i) == third && (!aSet.isEmpty())) {
+                for (int m = X; m <= Y; m++) {
+                    if (aSet.containsKey(i - m)) {
+                        result += aSet.get(i - m);
                     }
                 }
             }
