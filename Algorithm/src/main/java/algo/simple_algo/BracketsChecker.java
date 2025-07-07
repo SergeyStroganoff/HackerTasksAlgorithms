0ec694e95978fd8f90848f34a39f0d594a6252ae
Aspire5_Stroganow <111sergey90@gmail.com>
2025-07-07 08:24:14 -0400

    # new task - BracketsChecker

---
+++ Algorithm/src/main/java/algo/simple_algo/BracketsChecker.java
@@ -0,0 +1,42 @@
+package algo.simple_algo;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.Map;
+
+public class BracketsChecker {
+
+    public static void main(String[] args) {
+        String testOne = "()";
+        String testTwo = "(()";
+        String testThree = "())";
+        String testFour = "[(()))";
+        String testFive = "([{}])";
+        System.out.println(checkBrackets(testOne) ? "OK" : "FAIL");
+
+    }
+
+    private static boolean checkBrackets(String inputString) {
+        Map<Character, Character> bracketsMatchMap = new HashMap<>();
+        bracketsMatchMap.put(')', '(');
+        bracketsMatchMap.put(']', '[');
+        bracketsMatchMap.put('}', '{');
+        Deque<Character> charactersStack = new ArrayDeque<>();
+        for (int i = 0; i < inputString.length(); i++) {
+            char nextChar = inputString.charAt(i);
+            if (bracketsMatchMap.containsValue(nextChar)) {
+                charactersStack.push(nextChar);
+            } else {
+                if (bracketsMatchMap.containsKey(nextChar)) {
+                    if (!charactersStack.isEmpty() && charactersStack.peek().equals(bracketsMatchMap.get(nextChar))) {
+                        charactersStack.pop();
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }
+        return charactersStack.isEmpty();
+    }
+}
